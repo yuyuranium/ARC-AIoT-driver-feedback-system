95eaa2e0cbf5cb292598a766f7b40f685e0f868b
yuyuranium <e24076459@gs.ncku.edu.tw>
2021-08-02 00:38:10 +0800

    Optimize codes for labeling

--- arduino/bt_i2c_slave_data_collecter/bt_i2c_slave_data_collecter.ino
+++ arduino/bt_i2c_slave_data_collecter/bt_i2c_slave_data_collecter.ino
@@ -18,7 +18,7 @@ const char kMotionSymbols[kMotions] = { '_', '=', '+', '-', '<', '>' };
 
 unsigned long regret_trigger_time;
 uint8_t class_type;
-int file_counter;
+unsigned int file_counter;
 boolean busy;
 char val;
 
@@ -41,8 +41,6 @@ void setup() {
   delay(1000);
   
   Serial.begin(SERIAL_BAUD);
-
-  
   
   // himax eable setup
   pinMode(kHimaxPinOut, OUTPUT);
@@ -54,12 +52,9 @@ void setup() {
 
   // SD card setup
   while (!SD.begin(kSdPinOut)) {
-    // slowly blink 2 leds to indicate sd card error
-    // wait until no error
     LCDShowMsg("SD card", "Init failed");
     delay(500);
   }
-
   delay(200);
 
   // initialize variables
@@ -71,79 +66,91 @@ void setup() {
     sprintf(filename, "%d.csv", ++file_counter);
   } while (SD.exists(filename));  // initialize file_counter with latest file count
   LCDShowMsg("Label begin:", filename);
-  delay(1000);
   regret_trigger_time = millis();
 
   // bluetooth setup
   BTSerial.begin(BTSERIAL_BAUD);
+  unsigned long now, tick = millis();
+  do {
+    now = millis();
+    while (BTSerial.available())
+      val = BTSerial.read();
+  } while (now - tick < 1000);
+  LCDShowMsg("[P]", "Pending ...");
 }
 
 void loop() {
-  if (BTSerial.available()) {
-    val = BTSerial.read();
-    if (busy) {
-      if (val == 's') {
-        // disable himax, blow out the leds, write file and change state
-        digitalWrite(kHimaxPinOut, LOW);
-        data_entry.flush();
-        data_entry.close();
-        class_type = -1;
-        busy = false;
-        Serial.println("Done");
-        // LCDShowMsg("Writing", "Done");
-      } else if (val <= '5' && val >= '0') {
-        // reading class type, convert char to int
-        class_type = val - '0';
+  if (!BTSerial.available()) return;
+  
+  val = BTSerial.read();
+  if (busy) {
+    if (val == 's') {
+      // disable himax, blow out the leds, write file and change state
+      digitalWrite(kHimaxPinOut, LOW);
+      data_entry.flush();
+      data_entry.close();
+      class_type = -1;
+      busy = false;
+      Serial.println("Done");
+      LCDShowMsg("[P]", "Pending ...");
+    } else if (val <= '5' && val >= '0') {
+      // reading class type, convert char to int
+      class_type = val - '0';
+      lcd.setCursor(10, 1);
+      lcd.print(class_type);
+      lcd.setCursor(14, 0);
+      lcd.print(kMotionSymbols[class_type]);
+    }
+  } else {
+    if (val == 's') {
+      busy = false;  // remain the same
+    } else if (val == 'r' && debounce_regret()) {
+      // to avoid double clicking
+      // on regret, find the file and delete it
+      char filename[10], buf[17];
+      sprintf(filename, "%d.csv", file_counter - 1);
+      lcd.setCursor(0, 0);
+      lcd.print("[R]: ");
+      lcd.print(filename);
+      if (SD.exists(filename) && SD.remove(filename)) {
+        Serial.print("Deleting: ");
+        Serial.println(filename);
+        --file_counter;
         lcd.setCursor(0, 1);
-        lcd.print("Labeling: ");
-        lcd.print(class_type);
+        lcd.print("Done            ");
+      } else {
+        // fast blink 2 times to indicate error
+        lcd.setCursor(0, 1);
+        lcd.print("Failed          ");
       }
-    } else {
-      if (val == 's') {
-        busy = false;  // remain the same
-        LCDShowMsg("[P]", "Pending ...");
-      } else if (val == 'r' && debounce_regret()) {
-        // to avoid double clicking
-        // on regret, find the file and delete it
-        char filename[10], buf[17];
-        sprintf(filename, "%d.csv", file_counter - 1);
-        lcd.setCursor(0, 0);
-        lcd.print("[R]: ");
-        lcd.print(filename);
-        if (SD.exists(filename) && SD.remove(filename)) {
-          Serial.print("Deleting: ");
-          Serial.println(filename);
-          --file_counter;
-          lcd.setCursor(0, 1);
-          lcd.println("Done");
-          delay(1000);
-          // lit the led for 1 sec to indicate job done
-
-        } else {
-          // fast blink 2 times to indicate error
-          lcd.setCursor(0, 1);
-          lcd.println("Failed        ");
-          delay(1000);
-        }
-      } else if (val <= '5' && val >= '0') {
-        char filename[10];
-        do {
-          sprintf(filename, "%d.csv", file_counter++);
-        } while (SD.exists(filename));
-        data_entry = SD.open(filename, FILE_WRITE);
-        lcd.setCursor(0, 0);
-        lcd.print("[W]: ");
-        lcd.print(filename);
-        int i;
-        for (i = 0; i < kData - 1; ++i) {
-          data_entry.print(kDataName[i]);
-          data_entry.print(',');
-        }
-        data_entry.println(kDataName[i]);
-        class_type = val - '0';
-        busy = true;
-        digitalWrite(kHimaxPinOut, HIGH);
+      // non-blocking delay with consuming transmitted data
+      unsigned long now, tick = millis();
+      do {
+        now = millis();
+        while (BTSerial.available())
+          val = BTSerial.read();
+      } while (now - tick < 1000);
+      LCDShowMsg("[P]", "Pending ...");
+    } else if (val <= '5' && val >= '0') {
+      char filename[10];
+      do {
+        sprintf(filename, "%d.csv", file_counter++);
+      } while (SD.exists(filename));
+      data_entry = SD.open(filename, FILE_WRITE);
+      int i;
+      for (i = 0; i < kData - 1; ++i) {
+        data_entry.print(kDataName[i]);
+        data_entry.print(',');
       }
+      data_entry.println(kDataName[i]);
+      class_type = val - '0';
+      lcd.setCursor(0, 0);
+      lcd.print("[W]: ");
+      lcd.print(filename);
+      lcd.setCursor(0, 1);
+      lcd.print("Labeling: ");
+      busy = true;
+      digitalWrite(kHimaxPinOut, HIGH);
     }
   }
 }
@@ -158,12 +165,12 @@ void receiveEvent(int count) {
         unsigned char b = Wire.read();
         data.bytes[j] = b;
       }
-      Serial.print(data.f_val, 5);
+      // Serial.print(data.f_val, 5);
       data_entry.print(data.f_val, 5);
-      Serial.print(",");
+      // Serial.print(",");
       data_entry.print(",");
     }
-    Serial.println(class_type);
+    // Serial.println(class_type);
     data_entry.println(class_type);
   } else {
     // this should not happen, but just consume all unnecessary data in case it fails
